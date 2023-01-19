cc49b73e619b254d88106594baf318b13a8076cc
Alexander Banks <banksdev@Alexanders-MBP.webspeed.dk>
2023-01-19 08:47:18 +0100

    fix test

--- libs/design-system/src/lib/title/title.spec.tsx
+++ libs/design-system/src/lib/title/title.spec.tsx
@@ -1,32 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
-import { Button } from './title';
+import { Title } from './title';
 
-describe('Button', () => {
+describe('Title', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Button>Hello, World!</Button>);
+    const { baseElement } = render(<Title>Hello, World!</Title>);
     expect(baseElement).toBeTruthy();
   });
-
-  it('should render inner text', () => {
-    render(<Button>Hello, World!</Button>);
-
-    const button = screen.getByRole('button');
-    
-    expect(button.innerHTML).toContain('Hello, World!');
-  })
-
-  it('should fire click handler', () => {
-    const onClick = jest.fn(_ => null);
-
-    render(<Button onClick={onClick}>Hello, World!</Button>);
-
-    const button = screen.getByRole('button');
-
-    expect(onClick.mock.calls.length).toEqual(0);
-
-    button.click();
-    
-    expect(onClick.mock.calls.length).toEqual(1);
-  })
 });
